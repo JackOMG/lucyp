2630104722af9164d0b5f6f9a0095400d2d82bf1
Jack Smies <jacksmies@gmail.com>
2017-02-07 16:28:29 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const restify = require('restify');
+const builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.AppId,
+    appPassword: process.env.AppPassword,
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send(session.message.text);
+	//console.log(session.message.text)
+});
